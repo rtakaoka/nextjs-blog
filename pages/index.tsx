ddb46ee574e2b7411ee3784460090ce850ac440f
rtakaoka <31188845+rtakaoka@users.noreply.github.com>
2022-07-11 12:01:38 -0300

    Changing sorting on Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import { gql } from '@apollo/client'
 
 export const GET_POSTS = gql`
   query GetPosts {
-    posts(orderBy: publishedAt_DESC, stage: PUBLISHED) {
+    posts(orderBy: createdAt_DESC, stage: PUBLISHED) {
       id
       slug
       title
